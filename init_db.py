f180efc371fce57867e6b8f793877d1d2fbd6f8a
Chinedu Ezeokoye <nedu.steezy@gmail.com>
2023-03-04 01:30:38 +0000

    code+templates+db

---
+++ init_db.py
@@ -0,0 +1,80 @@
+import sqlite3
+import csv
+
+
+# open the connection to the database
+conn = sqlite3.connect('playstore_apps.db')
+cur = conn.cursor()
+
+# drop tables if they exist
+conn.execute('DROP TABLE IF EXISTS app')
+conn.execute('DROP TABLE IF EXISTS developer')
+conn.execute('DROP TABLE IF EXISTS category')
+conn.execute('DROP TABLE IF EXISTS ratings_reviews')
+
+# create tables
+conn.execute(
+    '''CREATE TABLE developer (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        developer_uid TEXT, 
+        developer_website TEXT, 
+        developer_email TEXT)''')
+conn.execute(
+    '''CREATE TABLE category (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        category_name TEXT)''')
+conn.execute(
+    '''CREATE TABLE app (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        app_name TEXT, 
+        app_identifier TEXT, 
+        app_version TEXT, 
+        app_size TEXT, 
+        release_date TEXT, 
+        app_rating REAL, 
+        developer_id INTEGER)''')
+conn.execute(
+    '''CREATE TABLE ratings_reviews (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        rating_count TEXT, 
+        reviews INTEGER,
+        app_id INTEGER,
+        FOREIGN KEY (app_id) REFERENCES app(id))''')
+
+# open the file to parse data and print to database
+with open('playstore_dataset.csv', newline='', encoding="utf8") as r:
+    reader = csv.reader(r, delimiter=",")
+    next(reader)
+    for row in reader:
+        app_name = row[0]
+        app_identifier = row[1]
+        app_version = row[28]
+        app_size = row[10]
+        app_rating = row[3]
+        release_date = row[15]
+        developer_id = int(row[12])
+        developer_uid = row[12]
+        developer_website = row[13]
+        developer_email = row[14]
+        rating_count = row[4]
+        reviews = row[23]
+        category_name = row[2] 
+
+        cur.execute('INSERT INTO app VALUES (NULL,?,?,?,?,?,?,?)',
+                    (app_name, app_identifier, app_version, app_size, release_date, app_rating, developer_id))
+        
+        app_id = cur.lastrowid
+        
+        cur.execute('INSERT INTO developer VALUES (NULL,?,?,?)', 
+                    (developer_uid, developer_website, developer_email))
+        
+        cur.execute('INSERT INTO ratings_reviews VALUES (NULL,?,?,?)', 
+                    (rating_count, reviews,app_id))
+        
+        
+        cur.execute('INSERT INTO category VALUES (NULL,?)', 
+                    (category_name,))
+
+# commit changes and close connection
+conn.commit()
+conn.close()
---
+++ main.py
@@ -0,0 +1,68 @@
+import sqlite3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/app')
+def application():
+
+    # open the connection to the database
+    conn = sqlite3.connect('playstore_apps.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("""SELECT *  FROM app""")
+    rows = cur.fetchall()
+
+    conn.close()
+
+    return render_template('app.html', rows=rows)
+
+@app.route('/developer')
+def developer():
+
+    # open the connection to the database
+    conn = sqlite3.connect('playstore_apps.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM developer""")
+    rows = cur.fetchall()
+
+    conn.close()
+
+    return render_template('developer.html', rows=rows)
+
+@app.route('/review')
+def review():
+
+    # open the connection to the database
+    conn = sqlite3.connect('playstore_apps.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute("""SELECT ratings_reviews.*, app.app_rating
+                    FROM ratings_reviews
+                    JOIN app ON ratings_reviews.app_id = app.id
+                """)
+    rows = cur.fetchall()
+
+    conn.close()
+
+    return render_template('review.html', rows=rows)
+
+@app.route('/app/<id>')
+def ratings(id):
+    conn = sqlite3.connect('playstore_apps.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    # get results from customers
+    cur.execute('SELECT app_rating FROM app WHERE app_id = ?', (id,))
+    rating = cur.fetchone()[0]
+    print(f"The rating for app {id} is {rating}")
+    conn.close()
+    return render_template('result.html', rating=rating)
+
+if __name__ == '__main__':
+    app.run(debug=True)
